8baf0458a614477307c76d31e8461bac1b4ff916
CloakingOcean <shawn.w8465@gmail.com>
2020-03-12 12:12:46 -0600

    Successfully corrected authentication and authorization

--- src/main/java/com/exitium/campaignchronicle/config/SecurityConfig.java
+++ src/main/java/com/exitium/campaignchronicle/config/SecurityConfig.java
@@ -1,22 +1,26 @@
 package com.exitium.campaignchronicle.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	UserDetailsService userDetailsService;
+	DataSource dataSource;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
+		auth.jdbcAuthentication()
+			.dataSource(dataSource);
 	}
 	
 	@Override
@@ -27,5 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Bean
-	public PasswordEncoder getPasswordEncoder() { return NoOpPasswordEncoder.getInstance(); }
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/com/exitium/campaignchronicle/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.exitium.campaignchronicle.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-						extends AbstractSecurityWebApplicationInitializer {
-
-}
